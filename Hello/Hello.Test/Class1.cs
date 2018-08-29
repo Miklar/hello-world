54fd9e31cccf27482566e98b38d5365f5bb21af5
Micke Larsson <micke@miklar.se>
2018-08-29 14:00:11 +0200

    Add basic mvc with test projects

---
+++ Hello/Hello.Test/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hello.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        //[Fact]
+        //public void TestThatShouldFail()
+        //{
+
+        //}
+    }
+}
